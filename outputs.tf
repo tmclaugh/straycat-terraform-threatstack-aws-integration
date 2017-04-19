f7864c8a88a7028061ef30281ef02400b50dfcf2
Tom McLaughlin <tom.mclaughlin@threatstack.com>
2017-04-18 21:07:56 -0400

    Switch this over to using the module.

--- outputs.tf
+++ outputs.tf
@@ -1,53 +1,53 @@
 // Outputs
 output "cloudtrail_id" {
-  value = "${module.aws_cloudtrail.cloudtrail_id}"
+  value = "${module.threatstack_aws_integration.cloudtrail_id}"
 }
 
 output "cloudtrail_home_region" {
-  value = "${module.aws_cloudtrail.cloudtrail_home_region}"
+  value = "${module.threatstack_aws_integration.cloudtrail_home_region}"
 }
 
 output "cloudtrail_arn" {
-  value = "${module.aws_cloudtrail.cloudtrail_arn}"
+  value = "${module.threatstack_aws_integration.cloudtrail_arn}"
 }
 
 output "iam_role_cloudtrail_arn" {
-  value = "${module.aws_cloudtrail.iam_role_cloudtrail_arn}"
+  value = "${module.threatstack_aws_integration.iam_role_cloudtrail_arn}"
 }
 
 output "cloudwatch_log_group_arn" {
-  value = "${module.aws_cloudtrail.cloudwatch_log_group_arn}"
+  value = "${module.threatstack_aws_integration.cloudwatch_log_group_arn}"
 }
 
 output "iam_role_name" {
-  value = "${aws_iam_role.role.name}"
+  value = "${module.threatstack_aws_integration.iam_role_name}"
 }
 
 output "iam_role_arn" {
-  value = "${aws_iam_role.role.arn}"
+  value = "${module.threatstack_aws_integration.iam_role_arn}"
 }
 
 output "s3_bucket_id" {
-  value = "${module.aws_cloudtrail.s3_bucket_id}"
+  value = "${module.threatstack_aws_integration.s3_bucket_id}"
 }
 
 output "s3_bucket_arn" {
-  value = "${module.aws_cloudtrail.s3_bucket_arn}"
+  value = "${module.threatstack_aws_integration.s3_bucket_arn}"
 }
 
 output "sns_topic_arn" {
-  value = "${aws_sns_topic.sns.arn}"
+  value = "${module.threatstack_aws_integration.sns_topic_arn}"
 }
 
 output "sqs_queue_id" {
-  value = "${aws_sqs_queue.sqs.id}"
+  value = "${module.threatstack_aws_integration.sqs_queue_id}"
 }
 
 output "sqs_queue_arn" {
-  value = "${aws_sqs_queue.sqs.arn}"
+  value = "${module.threatstack_aws_integration.sqs_queue_arn}"
 }
 
 output "sqs_queue_source" {
-  value = "${element(split(":", aws_sqs_queue.sqs.arn), 5)}"
+  value = "${module.threatstack_aws_integration.sqs_queue_source}"
 }
 
